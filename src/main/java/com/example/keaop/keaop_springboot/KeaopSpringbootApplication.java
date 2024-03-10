0c106c5315918a2e8367986428c38a69cb40b1d7
rupaksaikrishnayerramsetti <rupaksaikrishnayerramsetti@gmail.com>
2024-03-10 17:36:06 +0530

    Initial code repo

---
+++ src/main/java/com/example/keaop/keaop_springboot/KeaopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.keaop.keaop_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeaopSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeaopSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keaop/keaop_springboot/KeaopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keaop.keaop_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeaopSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
